0a2db35b60c763dcf4f1bb00d5470b92dc1f93e2
Ollie Rees <oliver.x.rees@gmail.com>
2025-12-14 21:41:14 +0000

    Remove Game

--- hangman/game.cpp
+++
@@ -1,9 +0,0 @@
-#include "game.hpp"
-
-void Game::guess_letter(char c) {
-    if (state->is_successful_guess(c)) {
-        state->add_correct_guess(c);
-    } else {
-        state->increment_wrong_guesses_count();
-    }
-}
