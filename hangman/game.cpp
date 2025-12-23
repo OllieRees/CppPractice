8ad42669f67a7ff26d43ca20860513cd75a780da
Ollie Rees <oliver.x.rees@gmail.com>
2025-12-23 04:45:09 +0000

    Update wrong_guesses to be after 'has_lost' check

--- hangman/game.cpp
+++ hangman/game.cpp
@@ -3,9 +3,9 @@
 
 State Game::make_guess(char c) {
     guesses.push_back(c);
+    if (!(this -> word -> contains_character(c))) { wrong_guesses++; };
     if (this->has_won()) { return State::win; }
     if (this->has_lost()) { return State::lose; }
-    if (!(this -> word -> contains_character(c))) { wrong_guesses++; };
     return State::in_progress;
 }
 
