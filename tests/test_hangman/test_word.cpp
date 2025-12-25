7ab1fd8d24fccacdfdda66b093c84132e957dd43
Ollie Rees <oliver.x.rees@gmail.com>
2025-12-25 03:48:10 +0000

    Assert set equality on tests

--- tests/test_hangman/test_word.cpp
+++ tests/test_hangman/test_word.cpp
@@ -1,12 +1,19 @@
 #include "word.hpp"
 #include <gtest/gtest.h>
+#include <set>
 
 TEST(TestWord, getCharactersNoDuplicates) {
   Word * word = new Word("team");
-  EXPECT_EQ(word->get_characters().size(), 4);
+  std::set<char> actual_char_set = word->get_characters();
+  std::set<char> expected_char_set = {'t', 'e', 'a', 'm'};
+  EXPECT_EQ(actual_char_set.size(), 4);
+  EXPECT_EQ(actual_char_set, expected_char_set);
 }
 
 TEST(TestWord, getCharactersWithDuplicates) {
   Word * word = new Word("hello");
+  std::set<char> actual_char_set = word->get_characters();
+  std::set<char> expected_char_set = {'h', 'e', 'l', 'o'};
   EXPECT_EQ(word->get_characters().size(), 4);
+  EXPECT_EQ(word->get_characters(), expected_char_set);
 }
