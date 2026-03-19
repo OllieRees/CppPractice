8ce14155d4ae68e0a64ef4bfc0b912c5e2625886
Ollie Rees <oliver.x.rees@gmail.com>
2026-03-19 01:56:35 +0000

    Use fixtures to reduce duplication

--- tests/test_hangman/test_display.cpp
+++ tests/test_hangman/test_display.cpp
@@ -4,80 +4,71 @@
 #include <iostream>
 #include <sstream>
 
-TEST(TestConsoleDisplayWord, NoGuesses) {
-    Game* game = new Game(new Word("hello"), 3);
-    
-    std::stringstream buffer;
-    std::streambuf* prevcoutbuf = std::cout.rdbuf();
-    std::cout.rdbuf(buffer.rdbuf());
+class TestConsoleDisplayWord: public testing::Test {
+    protected:
+        void SetUp() override {
+            this->prevcoutbuf = std::cout.rdbuf();
+            std::cout.rdbuf(this->buffer.rdbuf());
+        }
+        void TearDown() override {}
+    public:
+        std::stringstream buffer;
+        std::streambuf* prevcoutbuf;
+};
 
+TEST_F(TestConsoleDisplayWord, NoGuesses) {
+    Game* game = new Game(new Word("hello"), 3);
     Console(game).display_word();
 
-    std::cout.rdbuf(prevcoutbuf);
+    std::cout.rdbuf(this->prevcoutbuf);
 
-    ASSERT_EQ(buffer.str(), "_ _ _ _ _ \n");
+    ASSERT_EQ(this->buffer.str(), "_ _ _ _ _ \n");
 }
 
-TEST(TestConsoleDisplayWord, NoCorrectGuesses) {
+TEST_F(TestConsoleDisplayWord, NoCorrectGuesses) {
     Game* game = new Game(new Word("hello"), 3);
     game->make_guess('t');
-    
-    std::stringstream buffer;
-    std::streambuf* prevcoutbuf = std::cout.rdbuf();
-    std::cout.rdbuf(buffer.rdbuf());
 
     Console(game).display_word();
 
-    std::cout.rdbuf(prevcoutbuf);
+    std::cout.rdbuf(this->prevcoutbuf);
 
-    ASSERT_EQ(buffer.str(), "_ _ _ _ _ \n");
+    ASSERT_EQ(this->buffer.str(), "_ _ _ _ _ \n");
 }
 
-TEST(TestConsoleDisplayWord, FirstWordCorrect) {
+TEST_F(TestConsoleDisplayWord, FirstWordCorrect) {
     Game* game = new Game(new Word("hello"), 3);
     game->make_guess('h');
     
-    std::stringstream buffer;
-    std::streambuf* prevcoutbuf = std::cout.rdbuf();
-    std::cout.rdbuf(buffer.rdbuf());
-
     Console(game).display_word();
 
-    std::cout.rdbuf(prevcoutbuf);
+    std::cout.rdbuf(this->prevcoutbuf);
 
-    ASSERT_EQ(buffer.str(), "h _ _ _ _ \n");
+    ASSERT_EQ(this->buffer.str(), "h _ _ _ _ \n");
 }
 
-TEST(TestConsoleDisplayWord, MultipleCorrectGuesses) {
+TEST_F(TestConsoleDisplayWord, MultipleCorrectGuesses) {
     Game* game = new Game(new Word("hello"), 3);
     game->make_guess('h');
     game->make_guess('l');
 
-    std::stringstream buffer;
-    std::streambuf* prevcoutbuf = std::cout.rdbuf();
-    std::cout.rdbuf(buffer.rdbuf());
-
     Console(game).display_word();
 
-    std::cout.rdbuf(prevcoutbuf);
+    std::cout.rdbuf(this->prevcoutbuf);
 
-    ASSERT_EQ(buffer.str(), "h _ l l _ \n");
+    ASSERT_EQ(this->buffer.str(), "h _ l l _ \n");
 }
 
-TEST(TestConsoleDisplayWord, AllCorrectGuesses) {
+TEST_F(TestConsoleDisplayWord, AllCorrectGuesses) {
     Game* game = new Game(new Word("hello"), 3);
     game->make_guess('h');
     game->make_guess('l');
     game->make_guess('e');
     game->make_guess('o');
 
-    std::stringstream buffer;
-    std::streambuf* prevcoutbuf = std::cout.rdbuf();
-    std::cout.rdbuf(buffer.rdbuf());
-
     Console(game).display_word();
 
-    std::cout.rdbuf(prevcoutbuf);
+    std::cout.rdbuf(this->prevcoutbuf);
 
-    ASSERT_EQ(buffer.str(), "h e l l o \n");
+    ASSERT_EQ(this->buffer.str(), "h e l l o \n");
 }
